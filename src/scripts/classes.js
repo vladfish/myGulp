3848169b0c90a669d66cc46c166e82124b69cf65
vladfish <41798467+vladfish@users.noreply.github.com>
2022-06-08 14:07:24 +0400

    first version

---
+++ src/scripts/classes.js
@@ -0,0 +1,13 @@
+class Task {
+  constructor() {
+    console.log('constructor');
+  }
+
+  showId() {
+    console.log('23');
+  }
+
+  static loadAll() {
+    console.log('loadAll');
+  }
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+
+
+console.log(typeof Task);
+let task = new Task();
+task.showId();
+Task.loadAll();
